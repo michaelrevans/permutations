1448f387fb965187c5f61d106046e4829db3b780
Michael Evans <michael.evans@ride.capital>
2021-01-22 21:33:44 +0100

    Make function permutations rather than combinations

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::format;
 
 
-pub fn combinations(string: String) -> Vec<String> {
+pub fn permutations(string: String) -> Vec<String> {
     if string.len() == 1 {
         return vec![string];
     }
@@ -11,17 +11,9 @@ pub fn combinations(string: String) -> Vec<String> {
         .filter(|&s| s != "")
         .collect::<Vec<_>>();
 
-    println!("{:?}", letters);
-
     let mut result: Vec<String> = vec![];
 
     for index in 0..letters.len() {
-        // if letters[index] == "" {
-        //     continue;
-        // }
-
-        println!("{}", index);
-
         let mut other_letters = letters.clone();
 
         let letter = other_letters.remove(index);
@@ -32,13 +24,13 @@ pub fn combinations(string: String) -> Vec<String> {
             .collect::<Vec<_>>()
             .join("");
 
-        println!("other letters {}", other_letters);
-
-        for res in combinations(other_letters) {
+        for res in permutations(other_letters) {
             result.push(format!("{}{}", letter, res));
         }
     }
 
+    result.sort();
+    result.dedup();
     result
 }
 
@@ -48,22 +40,44 @@ mod tests {
 
     #[test]
     fn it_does_one_letter() {
-        let result = combinations(String::from("a"));
+        let result = permutations(String::from("a"));
 
-        assert_eq!(result, ["a"])
+        assert_eq!(result, ["a"]);
     }
 
     #[test]
     fn it_does_a_different_letter() {
-        let result = combinations(String::from("b"));
+        let result = permutations(String::from("b"));
 
         assert_eq!(result, ["b"]);
     }
 
     #[test]
     fn it_does_two_letters() {
-        let result = combinations(String::from("ab"));
+        let result = permutations(String::from("ab"));
 
-        assert_eq!(result, ["ab", "ba"])
+        assert_eq!(result, ["ab", "ba"]);
+    }
+
+    #[test]
+    fn it_does_many_letters() {
+        let mut result = permutations(String::from("abc"));
+        result.sort();
+
+        let mut expected_result = ["abc", "acb", "bac", "bca", "cab", "cba"];
+        expected_result.sort();
+
+        assert_eq!(result, expected_result);
+    }
+
+    #[test]
+    fn it_removes_duplicates() {
+        let mut result = permutations(String::from("aabb"));
+        result.sort();
+
+        let mut expected_result = ["aabb", "abba", "abab", "baab", "baba", "bbaa"];
+        expected_result.sort();
+
+        assert_eq!(result, expected_result);
     }
 }
